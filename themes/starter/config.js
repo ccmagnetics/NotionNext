bd18727de2958d934cdcaab8fb43553ac2ccbde0
CCmagnetics <ccmagnetics@gmail.com>
2026-01-22 17:10:35 +0800

    Update config.js
    新加关于我们

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -292,7 +292,7 @@ const CONFIG = {
     }
   ],
 
-  STARTER_FOOTER_SLOGAN: '我们通过技术为品牌和公司创造数字体验。',
+  STARTER_FOOTER_SLOGAN: 'CCmagnetics is a precision manufacturer of magnetic encoder rings and contactless transmission drives. We proudly supply high-performance magnetic solutions to renowned global brands in the automotive, electronics, R&D, and industrial sectors.',
 
   // 页脚三列菜单组
   STARTER_FOOTER_LINK_GROUP: [
