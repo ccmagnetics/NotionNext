fcb94a8088c142969ef3815de1483b2a582ed3c7
CCmagnetics <ccmagnetics@gmail.com>
2026-01-22 14:08:41 +0800

    Update config.js
    修改slogan

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -10,8 +10,8 @@ const CONFIG = {
 
   // 英雄区块导航
   STARTER_HERO_ENABLE: true, // 开启英雄区
-  STARTER_HERO_TITLE_1: '开源且免费的基于 Notion 笔记的网站构建工具', // 英雄区文字
-  STARTER_HERO_TITLE_2: '通过笔记无感知地建站、成倍放大您的价值', // 英雄区文字
+  STARTER_HERO_TITLE_1: 'Eliminate the $2,000 Per-Ring Tooling Cost.', // 英雄区文字
+  STARTER_HERO_TITLE_2: 'Stop waiting on prototyping costs. We provide direct access to 1,000+ production-ready magnetic ring molds, saving your R&D budget over $2,000 per iteration. Your breakthrough starts now.', // 英雄区文字
   // 英雄区两个按钮，如果TEXT留空则隐藏按钮
   STARTER_HERO_BUTTON_1_TEXT: '开始体验', // 英雄区按钮
   STARTER_HERO_BUTTON_1_URL:
